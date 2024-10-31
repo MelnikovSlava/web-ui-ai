0b952967a9107588d91c8a0eb3dc5f9323021bcf
slmel <jefferi@ya.ru>
2024-11-01 01:22:37 +0300

    Fix problem with static

--- frontend/rsbuild.config.ts
+++ frontend/rsbuild.config.ts
@@ -2,12 +2,9 @@ import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
 
 export default defineConfig({
-  plugins: [pluginReact()],
-  output: {
-    assetPrefix: ".",
-  },
-  html: {
-    favicon: "./src/assets/icon.png",
-    title: "Assistent",
-  },
+	plugins: [pluginReact()],
+	html: {
+		favicon: "./src/assets/icon.png",
+		title: "Assistent",
+	},
 });
