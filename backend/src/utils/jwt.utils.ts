4ae4f06d2b62b8bfcc6464751eda35b15bab874d
Slava Mel <jefferi@ya.ru>
2024-11-04 01:03:13 +0300

    Fix token lifetime

--- backend/src/utils/jwt.utils.ts
+++ backend/src/utils/jwt.utils.ts
@@ -2,7 +2,5 @@ import jwt from "jsonwebtoken";
 import { envVariables } from "../config";
 
 export const generateToken = (userId: number): string => {
-	return jwt.sign({ id: userId }, envVariables.jwtSecret, {
-		expiresIn: "30d",
-	});
+	return jwt.sign({ id: userId }, envVariables.jwtSecret, { expiresIn: '365d' });
 };
