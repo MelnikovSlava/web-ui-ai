3a2a07c8e2aa6b5059164f8b94626bf1f5a0ec92
Slava Mel <jefferi@ya.ru>
2024-11-04 01:26:47 +0300

    Fix mobile view

--- frontend/rsbuild.config.ts
+++ frontend/rsbuild.config.ts
@@ -6,5 +6,13 @@ export default defineConfig({
 	html: {
 		favicon: "./src/assets/icon.png",
 		title: "Assistent",
+		meta: {
+			"apple-mobile-web-app-capable": "yes",
+			"apple-mobile-web-app-status-bar-style": "black-translucent",
+			"apple-mobile-web-app-title": "Assistent",
+			"mobile-web-app-capable": "yes",
+			"theme-color": "#ffffff",
+			"viewport": "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+		}
 	},
 });
